1cadf71fb76597454d501c6e8f101df8fe522380
Harendra Kumar <110033700+developerdatascience@users.noreply.github.com>
2025-12-20 16:58:31 +0530

    Add session management for SQLite database

---
+++ pipelines/database/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dqp.database.base import Base
+
+ENGINE = create_engine("sqlite:///quality_metrics.db")
+SessionLocal = sessionmaker(bind=ENGINE)
+
+def init_db():
+    Base.metadata.create_all(bind=ENGINE)

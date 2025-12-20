5572f11bbcbe131dc489822ef2ce017b2ecceabb
Harendra Kumar <110033700+developerdatascience@users.noreply.github.com>
2025-12-20 16:58:01 +0530

    Add DataQualityMetric model to database

---
+++ pipelines/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.sql import func
+from dqp.database.base import Base
+
+
+class DataQualityMetric(Base):
+    __tablename__ = "data_quality_metrics"
+
+    id = Column(Integer, primary_key=True)
+    dataset_name = Column(String, index=True)
+    expectation_name = Column(String)
+    rule = Column(String)
+    total_records = Column(Integer)
+    failed_records = Column(Integer)
+    success_ratio = Column(Float)
+    status = Column(String)  # PASS / FAIL
+    created_at = Column(DateTime, server_default=func.now())
